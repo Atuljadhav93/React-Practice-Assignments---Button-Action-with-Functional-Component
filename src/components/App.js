80d1a1c3104e87b59558c6d98953ea0425a80d12
Atul <atuljadhav618@gmail.com>
2021-06-01 18:54:45 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,17 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const[isClicked,setToClicked] = useState(false);
+
+  const paraRender = ()=>{
+    setToClicked(true);
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={paraRender}>Click Me</button>
+      {isClicked ? <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+      </p>:null}
     </div>
   );
 }
